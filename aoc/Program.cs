e0fb5a69649b15633aeb92937a16c7a2bdd3abc1
spaceorc <spaceorc@yandex.ru>
2024-12-08 10:31:34 +0000

    Refactor

--- aoc/Program.cs
+++ aoc/Program.cs
@@ -27,32 +27,33 @@ public static class Program
         Part2().Out("Part 2: ");
         return;
 
-        long Part1() =>
-            AntennaPairs()
-                .SelectMany(vs => new[] { vs[0] * 2 - vs[1], vs[1] * 2 - vs[0] })
-                .Where(map.Inside)
-                .Distinct()
-                .Count();
-
-        long Part2() =>
-            AntennaPairs()
-                .SelectMany(
-                    vs => Enumerable
-                        .Range(0, int.MaxValue)
-                        .Select(n => vs[1] + (vs[1] - vs[0]) * n)
-                        .TakeWhile(map.Inside)
-                        .Concat(
-                            Enumerable
-                                .Range(0, int.MaxValue)
-                                .Select(n => vs[0] - (vs[1] - vs[0]) * n)
-                                .TakeWhile(map.Inside)
-                        )
-                )
-                .Distinct()
-                .Count();
-
         IEnumerable<V> Antennas() => map.All().Where(v => map[v] is not '.');
-        IEnumerable<List<V>> AntennaPairs() => Antennas().ToLookup(v => map[v]).SelectMany(g => g.ToArray().Combinations(2));
+
+        IEnumerable<List<V>> AntennaPairs() => Antennas()
+            .ToLookup(v => map[v])
+            .SelectMany(g => g.ToArray().Combinations(2));
+
+        long Part1() => AntennaPairs()
+            .SelectMany(vs => new[] { vs[0] * 2 - vs[1], vs[1] * 2 - vs[0] })
+            .Where(map.Inside)
+            .Distinct()
+            .Count();
+
+        long Part2() => AntennaPairs()
+            .SelectMany(
+                vs => Enumerable
+                    .Range(0, int.MaxValue)
+                    .Select(n => vs[1] + (vs[1] - vs[0]) * n)
+                    .TakeWhile(map.Inside)
+                    .Concat(
+                        Enumerable
+                            .Range(0, int.MaxValue)
+                            .Select(n => vs[0] - (vs[1] - vs[0]) * n)
+                            .TakeWhile(map.Inside)
+                    )
+            )
+            .Distinct()
+            .Count();
     }
 
     private static void Solve_7((long res, long[] args)[] input)
