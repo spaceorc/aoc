e0da4f1be4850cba047d396fb34887c16100c2cf
spaceorc <spaceorc@yandex.ru>
2024-12-09 07:29:40 +0000

    Day 9

--- aoc/Program.cs
+++ aoc/Program.cs
@@ -11,7 +11,8 @@ public static class Program
 {
     private static void Main()
     {
-        Runner.RunFile("day8.txt", Solve_8);
+        Runner.RunFile("day9.txt", Solve_9);
+        // Runner.RunFile("day8.txt", Solve_8);
         // Runner.RunFile("day7.txt", Solve_7);
         // Runner.RunFile("day6.txt", Solve_6);
         // Runner.RunFile("day5.txt", Solve_5);
@@ -19,6 +20,86 @@ public static class Program
         // Runner.RunFile("day3.txt", Solve_3);
         // Runner.RunFile("day2.txt", Solve_2);
         // Runner.RunFile("day1.txt", Solve_1);
+    }
+
+    private static void Solve_9(string input)
+    {
+        Part1().Out("Part 1: ");
+        Part2().Out("Part 2: ");
+        return;
+
+        long Part1()
+        {
+            var data = new List<int>();
+            var id = 0;
+            var isFree = false;
+            foreach (var c in input)
+            {
+                data.AddRange(isFree ? Enumerable.Repeat(-1, c - '0') : Enumerable.Repeat(id++, c - '0'));
+                isFree = !isFree;
+            }
+
+            var total = data.Count(x => x != -1);
+            var freeIndex = data.IndexOf(-1);
+            for (var i = data.Count - 1; i >= 0; i--)
+            {
+                if (data[i] == -1)
+                    continue;
+                (data[freeIndex], data[i]) = (data[i], data[freeIndex]);
+                freeIndex = data.IndexOf(-1, freeIndex + 1);
+                if (freeIndex >= total)
+                    break;
+            }
+
+            return data.Select((t, i) => t == -1 ? 0 : t * (long)i).Sum();
+        }
+
+        long Part2()
+        {
+            var freeBlocks = new List<(int size, int pos)>();
+            var files = new List<(int id, int size, int pos)>();
+            var id = 0;
+            var isFree = false;
+            var pos = 0;
+            foreach (var c in input)
+            {
+                if (isFree)
+                {
+                    if (c != '0')
+                    {
+                        freeBlocks.Add((c - '0', pos));
+                        pos += c - '0';
+                    }
+                }
+                else
+                {
+                    files.Add((id++, c - '0', pos));
+                    pos += c - '0';
+                }
+
+                isFree = !isFree;
+            }
+
+            var newFiles = new List<(int id, int size, int pos)>();
+            for (var i = files.Count - 1; i >= 0; i--)
+            {
+                var file = files[i];
+                var freeBlock = freeBlocks.Where(f => f.size >= file.size).MinBy(x => x.pos);
+                if (freeBlock == default || freeBlock.pos > file.pos)
+                {
+                    newFiles.Add(file);
+                    continue;
+                }
+
+                newFiles.Add(file with { pos = freeBlock.pos });
+                freeBlocks.Remove(freeBlock);
+
+                if (freeBlock.size > file.size)
+                    freeBlocks.Add((freeBlock.size - file.size, freeBlock.pos + file.size));
+            }
+
+            return newFiles.Select(f => Enumerable.Range(0, f.size).Select(i => (f.pos + i) * (long)f.id).Sum()).Sum();
+        }
     }
 
     private static void Solve_8(Map<char> map)
