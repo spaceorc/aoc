7f021b49aeb38e5eb1e062ec7abc80ae4391ead4
spaceorc <spaceorc@yandex.ru>
2024-12-17 09:36:06 +0000

    Refactor

--- aoc/aoc2024/day4/Day4.cs
+++ aoc/aoc2024/day4/Day4.cs
@@ -9,29 +9,28 @@ public class Day4(Map<char> input)
     {
         Part1().Out("Part 1: ");
         Part2().Out("Part 2: ");
-        return;
+    }
 
-        long Part1()
-        {
-            const string pattern = "XMAS";
-            return input
-                .All()
-                .Sum(start => V.area8.Count(d => pattern.All((c, i) => input.At(start + d * i) == c)));
-        }
+    private long Part1()
+    {
+        const string pattern = "XMAS";
+        return input
+            .All()
+            .Sum(start => V.area8.Count(d => pattern.All((c, i) => input.At(start + d * i) == c)));
+    }
 
-        long Part2()
+    private long Part2()
+    {
+        const string pattern = "MAS";
+        var allDirs = new V[][]
         {
-            const string pattern = "MAS";
-            var allDirs = new V[][]
-            {
-                [new V(-1, -1), new V(0, 0), new V(1, 1)],
-                [new V(-1, 1), new V(0, 0), new V(1, -1)],
-                [new V(1, -1), new V(0, 0), new V(-1, 1)],
-                [new V(1, 1), new V(0, 0), new V(-1, -1)],
-            };
-            return input
-                .All()
-                .Count(center => allDirs.Count(dirs => pattern.All((c, i) => input.At(center + dirs[i]) == c)) == 2);
-        }
+            [new V(-1, -1), new V(0, 0), new V(1, 1)],
+            [new V(-1, 1), new V(0, 0), new V(1, -1)],
+            [new V(1, -1), new V(0, 0), new V(-1, 1)],
+            [new V(1, 1), new V(0, 0), new V(-1, -1)],
+        };
+        return input
+            .All()
+            .Count(center => allDirs.Count(dirs => pattern.All((c, i) => input.At(center + dirs[i]) == c)) == 2);
     }
 }
