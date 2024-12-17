6632e6ecb0c53269b8f07b25cb7003057357bad8
spaceorc <spaceorc@yandex.ru>
2024-12-17 10:16:39 +0000

    Refactor

--- aoc/aoc2024/day6/Day6.cs
+++ aoc/aoc2024/day6/Day6.cs
@@ -5,36 +5,35 @@ namespace aoc.aoc2024.day6;
 
 public class Day6(Map<char> map)
 {
+    private readonly Walker start = new(map.All().Single(v => map[v] == '^'), Dir.Up);
+
     public void Solve()
     {
         Part1().Out("Part 1: ");
         Part2().Out("Part 2: ");
-        return;
-
-        long Part1() =>
-            Start()
-                .Walk(map, w => Next(w))
-                .Select(x => x.Pos)
-                .Distinct()
-                .Count();
-
-        long Part2() =>
-            Start()
-                .Walk(map, w => Next(w))
-                .Select(x => x.Pos)
-                .Distinct()
-                .Count(
-                    obstacle => Start()
-                        .WalkWithCycleCheck(map, w => Next(w, obstacle))
-                        .Last() is null
-                );
-
-
-        Walker Start() => new(map.All().Single(v => map[v] == '^'), Dir.Up);
-
-        Walker? Next(Walker w, V? obstacle = null) =>
-            !w.Forward().Inside(map) ? null
-            : w.Forward().Pos == obstacle || map[w.Forward().Pos] == '#' ? w.TurnCW()
-            : w.Forward();
     }
+
+    private long Part1() =>
+        start
+            .Walk(map, w => Next(w))
+            .Select(x => x.Pos)
+            .Distinct()
+            .Count();
+
+    private long Part2() =>
+        start
+            .Walk(map, w => Next(w))
+            .Select(x => x.Pos)
+            .Distinct()
+            .Count(
+                obstacle => start
+                    .WalkWithCycleCheck(map, w => Next(w, obstacle))
+                    .Last() is null
+            );
+
+
+    private Walker? Next(Walker w, V? obstacle = null) =>
+        !w.Forward().Inside(map) ? null
+        : w.Forward().Pos == obstacle || map[w.Forward().Pos] == '#' ? w.TurnCW()
+        : w.Forward();
 }
