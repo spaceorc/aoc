10f8b497f37fd66db067f72098317d1c03040271
spaceorc <spaceorc@yandex.ru>
2024-12-21 10:55:14 +0000

    Refactor

--- aoc/aoc2024/day21/Day21.cs
+++ aoc/aoc2024/day21/Day21.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using aoc.Lib;
 
 namespace aoc.aoc2024.day21;
@@ -24,17 +22,9 @@ public class Day21(string[] input)
         """
     );
 
-    private static readonly Dictionary<(string, int), long> cache2 = new();
-
     public void Solve()
     {
-        // foreach (var s in SolveFor(directional, "<A"))
-        // {
-        // Console.WriteLine(s);   
-        // }
-        // return;
         Solve(2).Out("Part 1: ");
-        // Solve(3).Out("Part 1: ");
         Solve(25).Out("Part 2: ");
     }
 
@@ -44,83 +34,38 @@ public class Day21(string[] input)
             .Select(x => x.sequence * long.Parse(x.code.Replace("A", "")))
             .Sum();
 
-    private static long SolveFor(string code, int directionals)
+    private static long SolveFor(string code, int directionalsCount) =>
+        GetAllSolutions(numeric, code)
+            .Select(s => GetSolutionLength(s, directionalsCount))
+            .Min();
+
+    private static long GetSolutionLength(string solution, int depth)
     {
-        var numericSolutions = GetAllSolutions(numeric, code).Distinct().ToList();
+        var cache = new Dictionary<(string, int), long>();
+        return Calc(solution, depth);
 
-        var all = new List<long>();
-        for (var ii = 0; ii < numericSolutions.Count; ii++)
+        long Calc(string s, int d)
         {
-            var numericSolution = numericSolutions[ii];
-            foreach (var current in GetAllSolutions(directional, numericSolution))
-            {
-                var res = CalcFor(current, directionals - 1);
-                // Console.WriteLine(res);
-                all.Add(res);
-            }
-        }
+            if (d == 0)
+                return s.Length;
 
-        return all.Min();
+            if (cache.TryGetValue((s, d), out var result))
+                return result;
+
+            var parts = s.Split('A').SkipLast(1).Select(x => x + "A").ToArray();
+            return cache[(s, d)] = parts.Length == 1
+                ? GetAllSolutions(directional, parts[0]).Select(sol => Calc(sol, d - 1)).Min()
+                : parts.Select(p => Calc(p, d)).Sum();
+        }
     }
 
-    private static long CalcFor(string solution, int depth)
-    {
-        if (cache2.TryGetValue((solution, depth), out var cached))
-            return cached;
-
-        if (depth == 0)
-            return solution.Length;
-
-        var parts = new List<string>();
-        var startIndex = 0;
-        while (startIndex < solution.Length)
-        {
-            var endIndex = solution.IndexOf('A', startIndex);
-            parts.Add(solution.Substring(startIndex, endIndex - startIndex + 1));
-            startIndex = endIndex + 1;
-        }
-
-        if (parts.Count == 0)
-            throw new Exception("Invalid solution");
-
-        var res = parts.Count > 1
-            ? parts.Select(x => CalcFor(x, depth)).Sum()
-            : GetAllSolutions(directional, parts[0]).Select(x => CalcFor(x, depth - 1)).Min();
-
-        cache2[(solution, depth)] = res;
-        return res;
-    }
-
-    private static List<string> GetAllSolutions(Map<char> keypad, string code)
-    {
-        var results = new List<StringBuilder> { new() };
-        foreach (var (s, e) in code.Prepend('A').Zip(code))
-        {
-            var solutions = GetAllPaths(keypad, s, e);
-            if (solutions.Length == 1)
-            {
-                foreach (var result in results)
-                    result.Append(solutions[0]);
-            }
-            else
-            {
-                var newResults = new List<StringBuilder>();
-                foreach (var result in results)
-                {
-                    foreach (var solution in solutions)
-                    {
-                        var newResult = new StringBuilder(result.ToString());
-                        newResult.Append(solution);
-                        newResults.Add(newResult);
-                    }
-                }
-
-                results = newResults;
-            }
-        }
-
-        return results.Select(x => x.ToString()).ToList();
-    }
+    private static string[] GetAllSolutions(Map<char> keypad, string code) =>
+        code.Prepend('A')
+            .Zip(code, (s, e) => GetAllPaths(keypad, s, e))
+            .Aggregate(
+                new[] { "" },
+                (acc, paths) => acc.SelectMany(a => paths.Select(p => a + p)).ToArray()
+            );
 
     private static string[] GetAllPaths(Map<char> keypad, char s, char e) =>
         Search.Bfs(
