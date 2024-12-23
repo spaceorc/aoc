c8925efc45d58c07c2630be651f9d68446bf6291
spaceorc <spaceorc@yandex.ru>
2024-12-23 18:13:23 +0000

    Introduce Bron–Kerbosch algorithm, refactor accordingly

--- aoc/Lib/Alg.cs
+++ aoc/Lib/Alg.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace aoc.Lib;
 
@@ -40,6 +42,54 @@ public static class Alg
         {
             cache[arg] = calc(arg, a => cache[a]);
             yield return (arg, cache[arg]);
+        }
+    }
+    
+     // Bron-Kerbosch algorithm with pivot
+     // https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+     // algorithm BronKerbosch2(R, P, X) is
+     //     if P and X are both empty then
+     //         report R as a maximal clique
+     //     choose a pivot vertex u in P ⋃ X
+     //     for each vertex v in P \ N(u) do
+     //         BronKerbosch2(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+     //         P := P \ {v}
+     //         X := X ⋃ {v}
+    public static IEnumerable<IReadOnlySet<T>> FindMaxCliques<T>(Dictionary<T, List<T>> graph) where T : notnull
+    {
+        var R = ImmutableHashSet<T>.Empty; // Current clique set
+        var P = graph.Keys.ToImmutableHashSet(); // Candidates
+        var X = ImmutableHashSet<T>.Empty; // Processed vertices
+
+        return BronKerboschWithPivot(graph, R, P, X);
+    }
+
+    private static IEnumerable<IReadOnlySet<T>> BronKerboschWithPivot<T>(
+        Dictionary<T, List<T>> graph,
+        ImmutableHashSet<T> R,
+        ImmutableHashSet<T> P,
+        ImmutableHashSet<T> X) where T : notnull
+    {
+        if (P.IsEmpty && X.IsEmpty)
+        {
+            yield return R;
+            yield break;
+        }
+
+        var pivot = P.Concat(X).MaxBy(v => graph[v].Count)!;
+        var candidates = P.Except(graph[pivot]);
+
+        foreach (var v in candidates)
+        {
+            var newR = R.Add(v);
+            var newP = P.Intersect(graph[v]);
+            var newX = X.Intersect(graph[v]);
+
+            foreach (var clique in BronKerboschWithPivot(graph, newR, newP, newX))
+                yield return clique;
+            
+            P = P.Remove(v);
+            X = X.Add(v);
         }
     }
 }
