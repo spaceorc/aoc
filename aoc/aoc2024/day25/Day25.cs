c3c6070fc8e368d9ee2273ea20eabf934aa3a6d9
spaceorc <spaceorc@yandex.ru>
2024-12-25 05:25:56 +0000

    Refactor

--- aoc/aoc2024/day25/Day25.cs
+++ aoc/aoc2024/day25/Day25.cs
@@ -8,12 +8,10 @@ public class Day25(params Map<char>[] input)
     public void Solve()
     {
         var lockCodes = input.Where(m => m[V.Zero] == '#')
-            .Select(m => m.Columns().Select(c => c.Count(v => m[v] == '#') - 1).ToArray())
-            .ToList();
+            .Select(m => m.Columns().Select(c => c.Count(v => m[v] == '#') - 1).ToArray());
 
         var keyCodes = input.Where(m => m[V.Zero] == '.')
-            .Select(m => m.Columns().Select(c => c.Count(v => m[v] == '#') - 1).ToArray())
-            .ToList();
+            .Select(m => m.Columns().Select(c => c.Count(v => m[v] == '#') - 1).ToArray());
 
         lockCodes
             .SelectMany(_ => keyCodes, (lockCode, keyCode) => (lockCode, keyCode))
